33972eb8511a5bf06a8206691bb4b0c2639e40eb
realkewal <kew4l@yahoo.com.au>
2018-01-15 11:44:29 +1100

    Server.test.js challenges

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ app.get('/todos/:id', (req, res) => {
         if (!todo) {
             res.status(404).send();
         }
-        res.status(200).send(todo);
+        res.status(200).send({todo});
     }).catch((e) => res.status(400).send());
 });
 
