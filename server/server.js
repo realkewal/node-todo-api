372ef2ae08967458fe7b16630efb5ef17e984e48
realkewal <kew4l@yahoo.com.au>
2018-01-16 16:16:49 +1100

    Added users route

--- server/server.js
+++ server/server.js
@@ -87,6 +87,19 @@ app.patch('/todos/:id', (req, res) => {
     })
 })
 
+//POST users use pick
+
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    var user = new User(body);
+    user.save().then((user) => {
+        res.send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
